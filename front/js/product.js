a9e855284494113acbe5b62473622be2e663e641
Lmahdi <ines.lmahdi@gmail.com>
2022-09-26 18:41:35 +0200

    First step page produit

--- front/js/product.js
+++ front/js/product.js
@@ -1,19 +1,47 @@
+let url = new URL('http://localhost:3000/api/products');
+let params = new URLSearchParams(url.search);
+
+
 fetch('http://localhost:3000/api/products')
     .then(response => {
         if (response.ok) {
             return response.json();
         }
 
-        throw new Error('Impossible de charger les produits');
+        throw new Error('Impossible de charger le produit');
     })
     .then(data => {
         for(const product of data) {
-            console.log(product.name, product._id);
+            console.log( params);
+
+
+            // IMAGE
+
+            /*const productImage = document.createElement('img');
+            productImage.src = product.imageUrl;
+            productImage.alt = product.altTxt;
+            item_img.append(productImage);*/
+
+            // TITRE
+
+            const productTitle = document.createElement('h1');
+            productTitle.textContent = product.name;
+            title.append(productTitle);
+
+            // DESCRIPTION
+
+            const productDescription = document.createElement('p');
+            productDescription.textContent = product.description;
+            description.append(productDescription);
+
+             // PRIX
+
+             const productPrice = document.createElement('span');
+             productPrice.textContent = product.price;
+             price.append(productPrice);
         }
 
-
-        })
-
+    })
     .catch(error => {
         console.log('toto', error.message);
     })
