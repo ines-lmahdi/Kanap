8f342bd6331d27c65b71ce2ec7de9b780f1feabe
Lmahdi <ines.lmahdi@gmail.com>
2022-10-07 11:36:35 +0200

    Implementation titre page

---
+++ front/js/cart.js
@@ -0,0 +1,24 @@
+/*
+
+PROPRIETES LOCALSTORAGE
+
+localStorage.setItem('');-------> Ajoute une entrée
+localStorage.getItem(); --------> Obtenir les données de l'élément depuis le stokage
+localStorage.length ; ----------> Retourne un entier representatn le nombre d'items stokés dans l'objet
+localStorage.removeItem(''); ---> Suppression d'un element
+localStorage.clear(); ----------> Suppression de tous les élémnents du localStorage.
+
+PROPRIETES STORAGE
+
+Storage.key(); ----------------> prend un nombre n en argument et retourne la n-ième clé contenue dans storage.
+Storage.getItem(); ------------> Lorqu'on lui passe le nom d'une clé, cette méthode renvoie la valeur de la clé correspondante.
+Storage.setItem(); ------------> Lorqu'on lui passe un nom de clé et une valeur, cette méthode ajoute cette clé et cette valeur dans le stockage.
+                                 Si la clé existe déjà, elle met à jour la valeur.
+Storage.removeItem(); ---------> Lorqu'on lui passe le nom d'une clé, cette méthode supprime cette clé du stockage.
+Storage.clear(); --------------> Lorsqu'elle est appelée, cette méthode supprime toutes les clés du stockage.
+Storage.length; ---------------> Renvoie un entier représentant le nombre d'éléments contenus dans l'objet Storage.
+
+
+Il faut faire un panier avec JSON
+
+*/
--- front/js/product.js
+++ front/js/product.js
@@ -1,3 +1,4 @@
+
 const produit = window.location.search.split("?id=").join("");
 console.log(produit);
 
@@ -11,148 +12,60 @@ const fetchProduit = async() => {
         produitData = promise ;
         console.log((produitData.name));
 
+         // IMPLEMENTATION TITRE PAGE
+
+        const productPageTitle = document.title;
+        document.title = produitData.name;
+
+
+        // IMPLEMENTATION IMAGE
+
         const productImage = document.createElement('img');
             productImage.src = produitData.imageUrl;
             productImage.alt = produitData.altTxt
             image.append(productImage);
 
+        // IMPLEMENTATION H1
 
         const productTitle = document.createElement('h1');
             productTitle.textContent = produitData.name;
             title.append(productTitle);
 
+         // IMPLEMENTATION DESCRIPTION
+
         const productDescription = document.createElement('p');
             productDescription.textContent = produitData.description;
             description.append(productDescription);
+
+         // IMPLEMENTATION PRIX
 
         const productPrice = document.createElement('span');
             productPrice.textContent = produitData.price;
             price.append(productPrice);
 
+         // IMPLEMENTATION OPTION COLOR
+
         const productColor1 = document.createElement('option');
             productColor1.text = produitData.colors[0];
             colors.append(productColor1);
 
-        const productColor2 = document.createElement('option');
-            productColor2.text = produitData.colors[1];
-            colors.append(productColor2);
-
-        const productColor3 = document.createElement('option');
-            productColor3.text = produitData.colors[2];
-            colors.append(productColor3);
+        // NOMBRE DE PRODUIT SELECTIONNÉ
 
 
+        // MESSAGE D'ALERTE
 
+        const addToCart = document.getElementById('addToCart');
+            addToCart.addEventListener('click', event => {
+                let quantityKanap = document.getElementById('itemQuantity');
+                window.alert('Félicitation ! Votre article '+ produitData.name +' '+ produitData.colors + ' ont été ajouté '+ quantity +' fois au panier !');
         });
-};
 
+    });
+
+}
 fetchProduit() ;
-/*
-const produitDisplay = async () => {
-    await fetchProduit();
-
-    document.getElementById("item").innerHTML = `
-    <article id="card${produitData._id}>
-        <div class="item__img" id="image">
-                <img src=" ${produitData.imageURL} " alt=" ${produitData.altTxt}">
-                </div>
-                <div class="item__content">
-                <div class="item__content__titlePrice">
-                    <h1 id="title"> ${produitData.name.toUpperCase()} </h1>
-                    <p>Prix : <span id="price"> ${produitData.price .toString() .replace(/0+$/, "")} </span>€</p>
-                </div>
-
-                <div class="item__content__description">
-                    <p class="item__content__description__title">Description :</p>
-                    <p id="description"> ${produitData.description} </p>
-                </div>
-
-                <div class="item__content__settings">
-                    <div class="item__content__settings__color">
-                    <label for="color-select">Choisir une couleur :</label>
-                    <select name="color-select" id="colors">
-                        <option value="">--SVP, choisissez une couleur --</option>
-                          <option value="vert">vert</option>
-                        <option value="blanc">blanc</option> -->
-                    </select>
-                    </div>
-    `;
-};
-
-produitDisplay();
-
 
 
 
 
 //https://www.youtube.com/watch?v=vsiajnM7Xuw&t=518s
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*let params = new
-URLSearchParams(window.location.search);
-let price = params.get("price");
-let title = params.get("name")
-console.log(params);
-
-
-// Recuperation d'une chaine de requete dans l'url
-
-const product =  window.location.search;
-console.log(product);
-
-fetch('http://localhost:3000/api/products')
-    .then(response => {
-        if (response.ok) {
-            return response.json();
-        }
-
-        throw new Error('Impossible de charger les produits');
-    })
-    .then(data => {
-        for(const product of data) {  // Possible erreur
-            console.log(product.name, product._id);
-
-            const productTitle = document.createElement('h1');
-            productTitle.textContent = product.name;
-            title.append(productTitle);
-
-            const productDescription = document.createElement('p');
-            productDescription.textContent = product.description;
-            description.append(productDescription);
-
-            const productPrice = document.createElement('span');
-            productPrice.textContent = (product.price/100);       // Possible erreur au niveau du texteContent
-            price.append(productPrice);
-
-            const productImage = document.createElement('img');
-            productImage.src = product.imageUrl;
-            productImage.alt = product.altTxt
-            image.append(productImage);
-        }
-    })
-    .catch(error => {
-        console.log('toto', error.message);
-    })
-}*/
