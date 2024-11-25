b0a4d79384d16ca9982c1d785c0067411540a824
Lmahdi <ines.lmahdi@gmail.com>
2024-11-25 10:33:17 +0100

    cleaning script.js

--- front/js/script.js
+++ front/js/script.js
@@ -1,3 +1,5 @@
+/*************************** Home page: product display **********************/
+
 fetch("http://localhost:3000/api/products")
   .then((response) => {
     if (response.ok) {
@@ -8,34 +10,29 @@ fetch("http://localhost:3000/api/products")
   })
   .then((data) => {
     for (const product of data) {
-      // console.log(product.name, product._id);
-
-      // LIEN
       const productLink = document.createElement("a");
+      const productArticle = document.createElement("article");
+      const productImage = document.createElement("img");
+      const productTitle = document.createElement("h3");
+      const productDescription = document.createElement("p");
 
+      // PRODUCT ID
       productLink.href = "./product.html?id=" + product._id;
       items.append(productLink);
-
-      //ARTICLE
-      const productArticle = document.createElement("article");
-
       productLink.append(productArticle);
 
       // IMAGE + TEXTE ALT
-      const productImage = document.createElement("img");
 
       productImage.src = product.imageUrl;
       productImage.alt = product.altTxt;
       productArticle.append(productImage);
 
-      // TITRE
-      const productTitle = document.createElement("h3");
+      // PRODUCT NAME
 
       productTitle.textContent = product.name;
       productArticle.append(productTitle);
 
       // DESCRIPTION
-      const productDescription = document.createElement("p");
 
       productDescription.textContent = product.description;
       productArticle.append(productDescription);
