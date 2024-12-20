dd5274e553cb2a2ef5dc81245ac4b6f05d516bf0
Lmahdi <ines.lmahdi@gmail.com>
2024-12-20 17:42:25 +0100

    fetch page index

--- front/js/index.js
+++ front/js/index.js
@@ -8,3 +8,31 @@ données trouver
 3/ Dans le catch, faire un message d'erreur dans le cas où l'api ne repond pas.
 
 */
+const items = document.querySelector("#items");
+
+// fetch("http://localhost:3000/api/products")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+fetch("http://localhost:3000/api/products")
+  .then((response) => response.json())
+  .then((data) => {
+    for (const product of data) {
+      const productArticle = document.createElement("article");
+
+      const productLink = document.createElement("a");
+      productLink.href = "./product.html?id=" + product._id;
+      items.append(productLink);
+      productLink.append(productArticle);
+
+      const productImage = document.createElement("img");
+      productImage.src = product.imageUrl;
+      productImage.alt = product.altTxt;
+      productArticle.append(productImage);
+
+      const productDescription = document.createElement("p");
+      productDescription.innerHTML = product.description;
+      productArticle.append(productDescription);
+    }
+  })
+  .catch((err) => console.log(err));
